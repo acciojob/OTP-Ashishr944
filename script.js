a2ec08aea0081ecdf1086807bcf2412b83412669
Ashish Rokade <ashishrokade944@gmail.com>
2026-09-14 17:39:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-//your JS code here. If required.
-const codes = documet.querySelectorAll(".code");
-codes[0].focus();
-codes.forEach((input, index) =>{
-	input.addEventListener("input", function (){
-		this.value = this.value.replace(/\D/g, "");
-		if(this.value && index < codes.length -1){
-			codes[index +1].focus();
-		}
-	});
+const codes = document.querySelectorAll(".code");
 
-	input.addEventListner("keydown", fucntion (event){
-		if(event.key === "Backspace"){
-			if(this.value === "" && index > 0){
-				codes[index -1].focus();
-				codes[index -1].value = "";
-			}
-		}
-	});
+codes[0].focus();
+
+codes.forEach((input, index) => {
+    input.addEventListener("input", function () {
+        this.value = this.value.replace(/\D/g, "");
+
+        if (this.value && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
+
+    input.addEventListener("keydown", function (event) {
+        if (event.key === "Backspace") {
+            if (this.value !== "") {
+                this.value = "";
+            } else if (index > 0) {
+                codes[index - 1].value = "";
+                codes[index - 1].focus();
+            }
+        }
+    });
 });
