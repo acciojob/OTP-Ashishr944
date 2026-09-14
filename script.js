87f5a4e661a31727d9772d3b2aae6be7d7b80627
Ashish Rokade <ashishrokade944@gmail.com>
2026-09-14 17:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const codes = documet.querySelectorAll(".code");
+codes[0].focus();
+codes.forEach((input, index) =>{
+	input.addEventListener("input", function (){
+		this.value = this.value.replace(/\D/g, "");
+		if(this.value && index < codes.length -1){
+			codes[index +1].focus();
+		}
+	});
+
+	input.addEventListner("keydown", fucntion (event){
+		if(event.key === "Backspace"){
+			if(this.value === "" && index > 0){
+				codes[index -1].focus();
+				codes[index -1].value = "";
+			}
+		}
+	});
+});
